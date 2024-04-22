55aefb3a8dbca05443247de07ece06d811a2a62a
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-04-21 22:17:02 -0300

    feat: context in react - context in form :sparkles:

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { History } from './pages/History'
 
-import { Home } from './pages/Home/Home'
+import { Home } from './pages/Home'
 
 export function Router() {
   return (
